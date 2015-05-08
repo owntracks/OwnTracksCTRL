1daf64289585d6d59ceaa98c6a058ed063fd593e
Christoph Krey <krey.christoph@gmail.com>
2015-05-08 11:54:19 +0200

    >Release date: 2015-05-08
    Preparing for App Store upgrade
    
    [NEW] Updated to newest fabric.io release
    [NEW] Updated to newest CocoaPods release
    
    >Release date: not released
    
    Updating internals
    
    [NEW] Use fabric.io crashlytcs
    [NEW] Use CocoaLumberjack for logging
    
    >Release date: 2015-04-09 for beta-testers only
    
    A few sanitary updates
    
    [NEW] Disabled Backgroundmode and Alarm/Event notifications
    [NEW] Upgrade to newest MQTT-Client
    [NEW] Use Launchscreen for congruent look on all devices
    [NEW] Use Foundation for Data to String conversion
    
    >Release date: 2015-02-12 App Store Version
    
    Bug fixes
    
    [FIX] Bugfix - crash on receive null payload

---
+++ CHANGELOG.md
@@ -0,0 +1,36 @@
+
+OwnTracksCTRL App 1.1 Release Notes
+===================================
+
+## OwnTracksCTRL 1.1.5
+>Release date: 2015-05-08
+
+Preparing for App Store upgrade
+
+[NEW] Updated to newest fabric.io release
+[NEW] Updated to newest CocoaPods release
+
+## OwnTracksCTRL 1.1.4
+>Release date: not released
+
+Updating internals
+
+[NEW] Use fabric.io crashlytcs
+[NEW] Use CocoaLumberjack for logging
+
+## OwnTracksCTRL 1.1.3
+>Release date: 2015-04-09 for beta-testers only
+
+A few sanitary updates
+
+[NEW] Disabled Backgroundmode and Alarm/Event notifications
+[NEW] Upgrade to newest MQTT-Client
+[NEW] Use Launchscreen for congruent look on all devices
+[NEW] Use Foundation for Data to String conversion
+
+## OwnTracksCTRL 1.1.2
+>Release date: 2015-02-12 App Store Version
+
+Bug fixes
+
+[FIX] Bugfix - crash on receive null payload 
--- OwnTracksCTRL/Pods/MQTTClient/MQTTClient/MQTTClient/MQTTSession.m
+++ OwnTracksCTRL/Pods/MQTTClient/MQTTClient/MQTTClient/MQTTSession.m
@@ -637,19 +637,23 @@
                                                 retainFlag:retainFlag
                                                    dupFlag:FALSE];
     if (qos) {
-        if (![self.persistence storeMessageForClientId:self.clientId
-                                            topic:topic
-                                             data:data
-                                       retainFlag:retainFlag
-                                              qos:qos
-                                            msgId:msgId
-                                           incomingFlag:NO]) {
+        MQTTFlow *flow = [self.persistence storeMessageForClientId:self.clientId
+                                                             topic:topic
+                                                              data:data
+                                                        retainFlag:retainFlag
+                                                               qos:qos
+                                                             msgId:msgId
+                                                      incomingFlag:NO];
+
+        if (!flow) {
             if (DEBUGSESS) NSLog(@"%@ dropping outgoing messages", self);
             msgId = 0;
         } else {
             [self tell];
             if ([self.persistence windowSize:self.clientId] <= self.persistence.maxWindowSize) {
-                [self send:msg];
+                if ([self send:msg]) {
+                    flow.deadline = [NSDate dateWithTimeIntervalSinceNow:DUPTIMEOUT];
+                }
             }
         }
     } else {
@@ -819,6 +823,7 @@
         }
     }
     for (MQTTFlow *flow in flows) {
+        if (DEBUGSESS)  NSLog(@"%@ %@ flow %@ %@ %@", self, self.clientId, flow.deadline, flow.commandType, flow.messageId);
         if ([flow.deadline compare:[NSDate date]] == NSOrderedAscending) {
             switch ([flow.commandType intValue]) {
                 case 0:
